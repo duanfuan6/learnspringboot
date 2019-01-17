5103d92140f7d07009dce7b9d163264c13c50590
duanfuan6 <zhangyilut@126.com>
2019-01-17 17:50:02 +0800

    2019/01/17

--- microservice-provider-user/src/main/java/com/itmuch/cloud/study/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/study/controller/UserController.java
@@ -1,21 +1,21 @@
 package com.itmuch.cloud.study.controller;
 
+import com.itmuch.cloud.study.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.itmuch.cloud.study.entity.User;
-import com.itmuch.cloud.study.repository.UserRepository;
+import com.itmuch.cloud.study.entity.SimpleUser;
 
 @RestController
 public class UserController {
   @Autowired
-  private UserRepository userRepository;
+  private IUserService userService;
 
   @GetMapping("/{id}")
-  public User findById(@PathVariable Long id) {
-    User findOne = this.userRepository.findOne(id);
+  public SimpleUser findById(@PathVariable Long id) {
+    SimpleUser findOne = this.userService.selectById(id);
     return findOne;
   }
 }
